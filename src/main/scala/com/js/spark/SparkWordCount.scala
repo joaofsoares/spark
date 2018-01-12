03b56f580e88d66c7619e877ad8de4e7e265d827
joaofsoares <joaofsoares@outlook.com>
2018-01-12 22:13:29 +0000

    code optimization

--- src/main/scala/com/js/spark/SparkWordCount.scala
+++ src/main/scala/com/js/spark/SparkWordCount.scala
@@ -2,17 +2,28 @@ package com.js.spark
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+import scala.reflect.io.File
+
 object SparkWordCount extends App {
 
-  val sparkConf = new SparkConf().setAppName("Spark Word Count").setMaster("local")
+  val InputFile = "in.txt"
+  val OutputDir = "out"
 
+  println(
+    if (File(OutputDir).deleteRecursively()) {
+      "Loading... cleaning directory... ready."
+    } else {
+      "Loading... ready."
+  })
+
+  val sparkConf = new SparkConf().setAppName("Spark Word Count").setMaster("local")
   val sc = new SparkContext(sparkConf)
 
-  val inputFile = sc.textFile("in.txt")
-
-  val count = inputFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
-
-  count.saveAsTextFile("out")
+  sc.textFile("in.txt")
+    .flatMap(line => line.split(" "))
+    .map(word => (word, 1))
+    .reduceByKey(_ + _)
+    .saveAsTextFile("out")
 
   sc.stop()
 
