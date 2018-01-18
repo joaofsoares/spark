2ccb5ff868348da93d7d69af36fdaa81fe53308f
joaofsoares <joaofsoares@outlook.com>
2018-01-18 12:36:18 +0000

    cleaning code

--- src/main/scala/SparkStreamingNetworkWordCount.scala
+++ src/main/scala/SparkStreamingNetworkWordCount.scala
@@ -17,10 +17,11 @@ object SparkStreamingNetworkWordCount extends App {
   //  val lines = ssc.socketTextStream("localhost", 9999)
   val lines = ssc.socketTextStream(args(0), args(1).toInt, StorageLevel.MEMORY_AND_DISK_SER)
 
-  val words = lines.flatMap(_.split(" "))
-  val wordCounts = words.map(x => (x, 1)).reduceByKey(_ + _)
-
-  wordCounts.print()
+  lines
+    .flatMap(_.split(" "))
+    .map(x => (x, 1))
+    .reduceByKey(_ + _)
+    .print()
 
   ssc.start()
   ssc.awaitTermination()
